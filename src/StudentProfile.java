305cddc1e6c48b61a034223c26becb942890aeb6
mggyslz <145471267+mggyslz@users.noreply.github.com>
2024-12-02 12:57:20 +0800

    Update StudentProfile.java

--- src/StudentProfile.java
+++ src/StudentProfile.java
@@ -5,23 +5,20 @@ class StudentProfile {
     private String studentName;
     private String studentID;
     private List<BorrowingRecord> borrowingRecords;
-    //Contructors
+  
     public StudentProfile(String studentName, String studentID) {
         this.studentName = studentName;
         this.studentID = studentID;
         this.borrowingRecords = new ArrayList<>();
     }
-
     public String getStudentName() { return studentName; }
     public String getStudentID() { return studentID; }
     public List<BorrowingRecord> getBorrowingRecords() { return borrowingRecords; }
-
-    // Method to add a borrowing record
+    public void setStudentName(String studentName) {this.studentName = studentName;}
+    public void setStudentID(String studentID) {this.studentID = studentID;}
     public void addBorrowingRecord(BorrowingRecord record) {
         borrowingRecords.add(record);
     }
-
-    // Method to display borrowing records
     public void displayBorrowingRecords() {
         if (borrowingRecords.isEmpty()) {
             System.out.println("No borrowing records for " + studentName);
