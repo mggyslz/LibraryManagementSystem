e5de2062429672bc2939796bcb5163ff9289b39a
mggyslz <145471267+mggyslz@users.noreply.github.com>
2024-12-02 12:58:10 +0800

    Update BorrowingRecord.java

--- src/BorrowingRecord.java
+++ src/BorrowingRecord.java
@@ -2,25 +2,15 @@ public class BorrowingRecord {
     private String bookTitle;
     private String borrowDate;
     private String returnDate;
-
+    
     public BorrowingRecord(String bookTitle, String borrowDate, String returnDate) {
         this.bookTitle = bookTitle;
         this.borrowDate = borrowDate;
         this.returnDate = returnDate;
     }
-
-    public String getBookTitle() {
-        return bookTitle;
-    }
-
-    public String getBorrowDate() {
-        return borrowDate;
-    }
-
-    public String getReturnDate() {
-        return returnDate;
-    }
-
+    public String getBookTitle() {return bookTitle;}
+    public String getBorrowDate() {return borrowDate;}
+    public String getReturnDate() {return returnDate;}
     public void setReturnDate(String returnDate) {
         this.returnDate = returnDate;
     }
