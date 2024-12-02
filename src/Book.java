6940bf22998f2424b71f0ba095fafc3634eb712d
mggyslz <145471267+mggyslz@users.noreply.github.com>
2024-12-02 12:56:16 +0800

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,26 +1,25 @@
 abstract class Book {
-    // Attributes
+    
     private String title;
     private String author;
     private String isbn;
     private boolean isBorrowed; 
-    // Constructor to initialize the book's details
+   
     public Book(String title, String author, String isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
         this.isBorrowed = false; 
     }
-    // Getter and Setter method
+
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
     public String getIsbn() { return isbn; }
     public boolean isBorrowed() { return isBorrowed; } 
-    public void setBorrowed(boolean borrowed) { isBorrowed = borrowed; } 
+
+    public void setBorrowed(boolean borrowed) { isBorrowed = borrowed; }
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
     
-    // Abstract method to display book details
-    // This must be implemented by any subclass of Book
     public abstract void displayDetails();
-    
-     
 }
